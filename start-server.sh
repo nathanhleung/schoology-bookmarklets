3af6aa11f362e516fda01a80f386cc2cf833f358
Nathan Leung <nathanhleung@users.noreply.github.com>
2017-11-09 09:04:50 -0500

    port 8008 isn't blocked

--- start-server.sh
+++ start-server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Starts a Python web server on port 8081
+# Starts a Python web server on port 8008
 
 # Based on https://gist.github.com/tdpreece/91c6b0305cc7a151e03f
 
-python -m SimpleHTTPServer 8081 &> /dev/null &
+python -m SimpleHTTPServer 8008 &> /dev/null &
 pid=$!
 
-echo "Server started on port 8081. PID: ${pid}."
+echo "Server started on port 8008. PID: ${pid}."
